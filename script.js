ff337890caf7b2a311a92022e11bccd0cf9fdcc4
Rtheodor <rafaeldacunhatheodoro@gmail.com>
2021-01-14 14:32:51 -0300

    Entendi como funciona: function/alert/ const=valor que nunca muda/click/addEventlistener/parseInt

---
+++ script.js
@@ -0,0 +1,24 @@
+/*var resposta = confirm("Aprendeu java script?");
+
+console.log(resposta);*/
+
+const a = 10;
+const b = 33;
+
+function somar(a,b){
+
+    return a+b;
+}
+
+/*console.log(somar(2, 5));*/
+
+document.querySelector("#calcular").addEventListener("click",function(){
+    let valorA = document.querySelector("#valorA").value;
+    let valorB = document.querySelector("#valorB").value;
+    if(valorA.length > 0 && valorB > 0){
+        alert(parseInt(valorA)+ parseInt(valorB));
+    }else{
+        alert('Por favor preencher os campos com numeros.');
+    }
+    
+});
